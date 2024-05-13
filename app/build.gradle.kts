143dad5f1faa0908c75ca8e53eeafc38a5701f4a
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-05-13 21:22:16 +0300

    movie items layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
+//apply plugin: 'com.android.application'
+//apply plugin: 'com.google.dagger.hilt.android'
+
 
 android {
     namespace = "com.winfred.pagingapp"
@@ -59,6 +62,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,7 +74,32 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    //retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson")
+
+
+
+    //paging library
+    implementation ("androidx.paging:paging-runtime:3.2.1")
+    implementation("androidx.paging:paging-rxjava3:3.2.1")
+
+    //Hilt Dagger
+    implementation ("com.google.dagger:hilt-android:2.51.1")
+    annotationProcessor ("com.google.dagger:hilt-compiler:2.51.1")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //lifecycle
+    val lifecycle_version = "2.7.0"
+    val arch_version = "2.2.0"
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    //
 }
