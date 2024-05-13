8d40e138bb93c2815b3d0c2ee75dc90aa9c99627
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-05-13 21:32:13 +0300

    model classes

---
+++ app/src/main/java/com/winfred/pagingapp/model/Movie.java
@@ -0,0 +1,44 @@
+package com.winfred.pagingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("vote_average")
+    @Expose
+    private double voteAverage;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
